e3ff95ad18d35d571da57812d7ef77b7f071c8b4
tsucchinoko <ccya2211@icloud.com>
2025-11-26 20:36:08 +0900

    chore: init biome

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
+import { invoke } from "@tauri-apps/api/core";
 
-  let name = $state("");
-  let greetMsg = $state("");
+const name = $state("");
+let greetMsg = $state("");
 
-  async function greet(event: Event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
-  }
+async function greet(event: Event) {
+	event.preventDefault();
+	// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+	greetMsg = await invoke("greet", { name });
+}
 </script>
 
 <main class="container">
